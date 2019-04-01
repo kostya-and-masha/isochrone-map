ed93014ab4739ada862edb1a79822cc4a1f6689e
Konstantin Lyubort <lyubortk@gmail.com>
2019-04-01 05:18:29 +0300

    merged MainActivity and MapsActivity

--- app/src/main/java/com/example/isochronemap/MainActivity.java
+++ app/src/main/java/com/example/isochronemap/MainActivity.java
@@ -8,11 +8,20 @@ import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
     private boolean menuButtonIsActivated = false;
     private SearchView searchField;
     private ConstraintLayout menuSection;
@@ -34,6 +43,39 @@ public class MainActivity extends AppCompatActivity {
         carButton = findViewById(R.id.car_button);
         TransportButton(walkingButton);
         closeSettings();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng am = new LatLng(59.980547, 30.324066);
+        mMap.addMarker(new MarkerOptions().position(am).title("Marker in AM"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(am, 10));
+
+        PolygonOptions rectOptions = new PolygonOptions()
+                .strokeWidth(1)
+                .strokeColor(0xffbb0000)
+                .fillColor(0x66ff0000)
+                .add(
+                        new LatLng(59.9, 30.3),
+                        new LatLng(60, 30.3),
+                        new LatLng(60, 30.4)
+                );
+        mMap.addPolygon(rectOptions);
     }
 
     public void TransportButton(View view) {
